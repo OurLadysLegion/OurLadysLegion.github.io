5d5e582fee598e136e3799f04f787c6dcbff285f
Nicholas Gillroy <104165073+BuddyRaider@users.noreply.github.com>
2025-04-11 20:21:41 -0400

    Update navBar.js

--- docs/navBar.js
+++ docs/navBar.js
@@ -1,6 +1,8 @@
 const sidebarMenu = document.getElementById("sidebarMenu");
 const sidebar = document.getElementById("sidebar");
 const navMenu = document.getElementById("navMenu");
+const navText = document.getElementById("navText");
+const navLogo = document.getElementById("navLogo");
 
 navMenu.addEventListener("click", (evt) => {
 	sidebar.style.transform = "translateX(calc(-100% + 5px)";
@@ -8,4 +10,12 @@ navMenu.addEventListener("click", (evt) => {
 
 sidebarMenu.addEventListener("click", (evt) => {
 	sidebar.style.transform = "none";
+});
+
+navText.addEventListener("click", (evt) => {
+    location.pathname = "/";
+});
+
+navLogo.addEventListener("click", (evt) => {
+    location.pathname = "/";
 });
