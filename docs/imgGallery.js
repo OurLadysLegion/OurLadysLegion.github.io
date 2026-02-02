5dc477827dd524567c603ebeddbb321dfbfbc666
Nick Gillroy <nickgillroy@gmail.com>
2026-02-02 10:38:24 -0500

    Added Image Gallery Pages

---
+++ docs/imgGallery.js
@@ -0,0 +1,17 @@
+const imgGallery = document.querySelectorAll("div.imgGallery");
+
+for (let i=0; i<imgGallery.length; i++) {
+    let imgs = imgGallery[i].querySelectorAll("div.column img");
+    for (let j=0; j<imgs.length; j++) {
+        imgs[j].addEventListener("click", (evt) => {
+            const img = evt.target;
+            if (img.tagName === "IMG") {
+                if (document.fullscreenElement) {
+                    document.exitFullscreen();
+                } else {
+                    img.requestFullscreen();
+                }
+            }
+        });
+    }
+}
