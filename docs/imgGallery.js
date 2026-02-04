2a9f5f67b6778eda7f3da08365360eb33db90588
Nick Gillroy <nickgillroy@gmail.com>
2026-02-04 11:16:45 -0500

    Updated Image Gallery with a Caching Workaround
    Improves chances of working on low-end devices like iPhones

--- docs/imgGallery.js
+++ docs/imgGallery.js
@@ -3,6 +3,7 @@ const imgGallery = document.querySelectorAll("div.imgGallery");
 for (let i=0; i<imgGallery.length; i++) {
     let imgs = imgGallery[i].querySelectorAll("div.column img");
     for (let j=0; j<imgs.length; j++) {
+        imgs[j].src += "?_=" + Math.random();
         imgs[j].addEventListener("click", (evt) => {
             const img = evt.target;
             if (img.tagName === "IMG") {
